fca3e6ed77a61c5d0b452040ed4414908a67a3fd
SherlySusan37 <159609429+SherlySusan37@users.noreply.github.com>
2025-01-04 14:00:30 +0530

    Create ProductManagement.js

---
+++ ProductManagement.js
@@ -0,0 +1,6 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './ProductManagement.css';
+
+const ProductManagement = () => { /* Code as previously shared */ };
+export default ProductManagement;
